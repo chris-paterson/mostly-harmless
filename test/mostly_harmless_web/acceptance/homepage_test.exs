ac23306de381b2687928934aebebf7f093c10547
Chris Paterson <CJPaterson.3@gmail.com>
2017-09-13 19:25:26 -0400

    Added homepage test and make it pass.

---
+++ test/mostly_harmless_web/acceptance/homepage_test.exs
@@ -0,0 +1,11 @@
+defmodule MostlyHarmlessWeb.HomepageTest do
+  use ExUnit.Case
+  use Hound.Helpers
+
+  hound_session()
+
+  test "presence of featured products" do
+    navigate_to("/")
+    assert page_source() =~ "Seasonal Products"
+  end
+end
